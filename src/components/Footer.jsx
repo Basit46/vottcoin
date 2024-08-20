b93b79245524636a023c1dcaa91aac569da1ec74
basit <hassanbasit1990@gmail.com>
2024-08-20 18:20:08 +0100

    Added changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-scroll";
+import linkBg from "../assets/linkBg.svg";
+import { FaFacebookF, FaPlay } from "react-icons/fa";
+import { FaXTwitter, FaInstagram, FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -13,7 +16,7 @@ const Footer = () => {
           <p>All Rights Reserved.</p>
         </div>
 
-        <div>
+        <div className="w-[80%] flex flex-col items-center">
           <div className="font-[600] font-Rajdhani gap-[30px] flex items-center uppercase">
             <Link to="Hero">HOME</Link>
             <Link to="Hero">Features</Link>
@@ -23,10 +26,54 @@ const Footer = () => {
             <Link to="Hero">CONTACT</Link>
           </div>
 
-          <div className="mt-[50px] border-y-[4px] border-[#0F1C23]">
+          <div className="mt-[50px] w-full border-y-[3px] border-[#0F1C23] py-[20px] flex justify-between items-center">
             <div>
-              <p>Office Location:</p>
-              <p>dubai</p>
+              <p className="text-[24px] font-Rajdhani font-[600]">
+                Office <span className="text-[#6850FF]">Location:</span>
+              </p>
+              <p className="font-Poppins text-[#969EB2]">dubai</p>
+            </div>
+
+            <div>
+              <p className="text-[24px] text-center font-Rajdhani font-[600]">
+                Contact <span className="text-[#6850FF]">Information:</span>
+              </p>
+              <a
+                href="http://vottcoin.io/"
+                target="blank"
+                className="font-Poppins text-center mx-auto block w-fit text-[#969EB2]"
+              >
+                vottcoin.io
+              </a>
+            </div>
+
+            <div>
+              <p className="text-[24px] text-center font-Rajdhani font-[600]">
+                Join <span className="text-[#6850FF]">Vottcoin</span> Community
+              </p>
+
+              <div className="flex gap-[10px]">
+                <div className="size-[46px] relative grid place-items-center">
+                  <img className="size-full" src={linkBg} alt="linkBg" />
+                  <FaFacebookF className="absolute text-[24px]" />
+                </div>
+                <div className="size-[46px] relative grid place-items-center">
+                  <img className="size-full" src={linkBg} alt="linkBg" />
+                  <FaXTwitter className="absolute text-[24px]" />
+                </div>
+                <div className="size-[46px] relative grid place-items-center">
+                  <img className="size-full" src={linkBg} alt="linkBg" />
+                  <FaInstagram className="absolute text-[24px]" />
+                </div>
+                <div className="size-[46px] relative grid place-items-center">
+                  <img className="size-full" src={linkBg} alt="linkBg" />
+                  <FaLinkedin className="absolute text-[24px]" />
+                </div>
+                <div className="size-[46px] relative grid place-items-center">
+                  <img className="size-full" src={linkBg} alt="linkBg" />
+                  <FaPlay className="absolute text-[24px]" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
