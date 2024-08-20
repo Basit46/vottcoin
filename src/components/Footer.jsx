798ee59b49b42b36159eef07c37ab9d281d7430e
basit <hassanbasit1990@gmail.com>
2024-08-20 17:34:58 +0100

    Added changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="bg-[#6850FF] h-[50vh] w-full mt-[40px]">Footer</div>;
+  return (
+    <footer className="bg-[#6850FF] h-[613px] w-full mt-[80px] pb-[30px]">
+      <div className="relative h-full w-full bg-black">
+        <div className="clip1 absolute top-0 left-1/2 -translate-x-1/2 bg-[#6850FF] w-[400px] h-[100px] grid place-items-center">
+          <p className="text-[80px] font-Boogaloo leading-none">Vottcoin</p>
+        </div>
+        <div className="clip2 px-[20px] absolute bottom-[-2px] left-1/2 -translate-x-1/2 bg-[#6850FF] w-[400px] h-[40px] flex justify-center gap-[20px] items-center">
+          <p>Copyright 2024</p>
+          <p>All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
