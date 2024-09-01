64315f86702223edf2e1bb9ee4275d04318fea36
basit <hassanbasit1990@gmail.com>
2024-09-02 00:35:47 +0100

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <MobileMenu open={open} setOpen={setOpen} />
       <Hero />
       <iframe
-        className="w-[90%] sm:w-[500px] h-[1400px] sm:h-[1110px] mx-auto my-[30px]"
+        className="w-[90%] sm:w-[500px] h-[1480px] vsm:h-[1400px] sm:h-[1110px] mx-auto my-[30px]"
         src="https://vottcoin-widget.vercel.app/"
         frameborder="0"
       ></iframe>
