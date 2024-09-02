c5b49e6e658cde63b8873cd7f567d251fbe9af10
basit <hassanbasit1990@gmail.com>
2024-09-02 23:19:43 +0100

    New version

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <Navbar open={open} setOpen={setOpen} />
       <MobileMenu open={open} setOpen={setOpen} />
       <Hero />
-      <div className="my-[50px]">
+      <div className="my-[50px] w-[90%] mx-auto">
         <Widget />
       </div>
       <About />
