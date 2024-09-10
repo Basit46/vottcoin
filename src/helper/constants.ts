120241963530b899d6f837d8833aeb48d7edb112
Farman Shaik <shkfrmn786@gmail.com>
2024-09-10 22:00:55 +0530

    re add public folder and change to mainnet

--- src/helper/constants.ts
+++ src/helper/constants.ts
@@ -1,6 +1,6 @@
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl, Connection } from "@solana/web3.js";
 
-export const network = WalletAdapterNetwork.Devnet;
+export const network = WalletAdapterNetwork.Mainnet;
 export const connection = new Connection(clusterApiUrl(network), "confirmed");
 export const host = 'http://localhost:5000';
